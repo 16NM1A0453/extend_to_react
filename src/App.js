bfe194bd282a40e78b23aea6487da6b0d822fcb3
16NM1A0453 <niharika.gunnu1999@gmail.com>
2024-08-04 19:07:25 +0530

    code clean commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { useReducer } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavBar from "./Components/NavBar/NavBar"
-import Home from "./Components/Home/Home";
-import Skills from "./Components/Skills/Skills";
-import Contact from "./Components/Contact/Contact";
 
 function App() {
   return ( 
     <>
-    <NavBar />
-    <Home />
-    <Skills />
-    <Contact />
+    
     </>
   );
 }
